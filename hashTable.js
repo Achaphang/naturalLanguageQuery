0f13a89093cceb40fd60ad9bb92ea53e94a0d44c
Achaphang <43050805+Achaphang@users.noreply.github.com>
2020-05-04 18:55:52 -0700

    Function Relocation

--- hashTable.js
+++ hashTable.js
@@ -33,8 +33,69 @@ optionalQueries = addNewQueries(input, optionalQueries);
 console.log(optionalQueries);
 
 /*******************************************************************************
- * All the functionality between this comment and the next is for hash tables.
- * It all came from here:
+ * addNewQueries
+ * The first argument is the string we want to use to add keys and values
+ * our hash table. The second argument is our hash table.
+ ******************************************************************************/
+function addNewQueries(addInputQueries, possibleQueries) {
+  var keyStr; // This is used insert keys
+  var valArr; // This is used to insert values associated with keys
+
+  // We break up the string into tokens to index for setting
+  // keys and values using token indices
+  var tokens = tokenize.tokenize(addInputQueries);
+  // The outer loop controls the range of tokens we are looking at
+  for(var i = 0; i < tokens.length - 1; i++) {
+    keyStr = "";  // Resets the string back to empty
+    valArr = [];  // Resets the array back to empty
+    // The inner loop looks one ahead of i to add the value we want for our key
+    for(var j = 0; j <= i + 1; j++) {
+      // The first conditional adds a space when we have multiple strings
+      // we want to use as a key
+      if(j <= i && i != j) {
+        keyStr += (tokens[j] + " ");
+      }
+      // The second conditional adds the last string we want to use as a key
+      else if(i == j && i != tokens.length) {
+        keyStr += tokens[j].toString();
+      }
+      // The third condition pushes the value onto the array that we associated
+      // with the key value
+      else{
+        valArr.push(tokens[j]);
+      }
+    }
+
+    // After setting up our keys and values we check to see if they are already
+    // in the hash table. If they don't exist already then they are just added
+    // to the hash table. If a key already exists we see if the value
+    // we want to add exists in the values associated with that key. If there
+    // is no association between that value and the key we push all the other
+    // values onto the array. We update the hash table with the key and values
+    // we updated
+
+    // First conditional checks if we have this key already
+    if(possibleQueries.keys().includes(keyStr)) {
+      // Second conditional checks if the value is associated with that key
+      if(!(possibleQueries.getItem(keyStr).includes(valArr[0]))) {
+        // This loops through our already existing values to push them on our
+        // value array
+        for(var k = 0; k <= possibleQueries.getItem(keyStr).length - 1; k++)
+          valArr.push(possibleQueries.getItem(keyStr)[k]);
+      }
+    }
+    // Assign keys and values to the hash table
+    possibleQueries.setItem(keyStr, valArr);
+  }
+
+  // This returns our hash table after the outer for loop is done to use for
+  // future operations
+  return possibleQueries;
+}
+
+/*******************************************************************************
+ * All the functionality below this comment is for hash tables
+ * It all came from this website:
  * http://www.mojavelinux.com/articles/javascript_hashes.html
  *******************************************************************************/
 function HashTable(obj)
@@ -118,65 +179,4 @@ function HashTable(obj)
         this.items = {}
         this.length = 0;
     }
-}
-
-/*******************************************************************************
- * addNewQueries
- * The first argument is the string we want to use to add keys and values
- * our hash table. The second argument is our hash table.
- ******************************************************************************/
-function addNewQueries(addInputQueries, possibleQueries) {
-  var keyStr; // This is used insert keys
-  var valArr; // This is used to insert values associated with keys
-
-  // We break up the string into tokens to index for setting
-  // keys and values using token indices
-  var tokens = tokenize.tokenize(addInputQueries);
-  // The outer loop controls the range of tokens we are looking at
-  for(var i = 0; i < tokens.length - 1; i++) {
-    keyStr = "";  // Resets the string back to empty
-    valArr = [];  // Resets the array back to empty
-    // The inner loop looks one ahead of i to add the value we want for our key
-    for(var j = 0; j <= i + 1; j++) {
-      // The first conditional adds a space when we have multiple strings
-      // we want to use as a key
-      if(j <= i && i != j) {
-        keyStr += (tokens[j] + " ");
-      }
-      // The second conditional adds the last string we want to use as a key
-      else if(i == j && i != tokens.length) {
-        keyStr += tokens[j].toString();
-      }
-      // The third condition pushes the value onto the array that we associated
-      // with the key value
-      else{
-        valArr.push(tokens[j]);
-      }
-    }
-
-    // After setting up our keys and values we check to see if they are already
-    // in the hash table. If they don't exist already then they are just added
-    // to the hash table. If a key already exists we see if the value
-    // we want to add exists in the values associated with that key. If there
-    // is no association between that value and the key we push all the other
-    // values onto the array. We update the hash table with the key and values
-    // we updated
-
-    // First conditional checks if we have this key already
-    if(possibleQueries.keys().includes(keyStr)) {
-      // Second conditional checks if the value is associated with that key
-      if(!(possibleQueries.getItem(keyStr).includes(valArr[0]))) {
-        // This loops through our already existing values to push them on our
-        // value array
-        for(var k = 0; k <= possibleQueries.getItem(keyStr).length - 1; k++)
-          valArr.push(possibleQueries.getItem(keyStr)[k]);
-      }
-    }
-    // Assign keys and values to the hash table
-    possibleQueries.setItem(keyStr, valArr);
-  }
-
-  // This returns our hash table after the outer for loop is done to use for
-  // future operations
-  return possibleQueries;
 }
