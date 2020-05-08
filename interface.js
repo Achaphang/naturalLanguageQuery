04fc2843e099c6461d69f373d57e21c4bc2e4683
DevDrako <45469673+DevDrako@users.noreply.github.com>
2020-05-07 18:54:29 -0700

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,6 +1,18 @@
 var http = require('http');
 var dt = require('./hashTable.js');
 var uc = require('natural');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost:3306",
+  user: "root",
+  password: "CS360"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
