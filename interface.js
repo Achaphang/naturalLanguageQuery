94d254daaf31b18bbd79d0a91940bb131fdd3e6f
Achaphang <43050805+Achaphang@users.noreply.github.com>
2020-05-07 20:08:36 -0700

    Updates

--- interface.js
+++ interface.js
@@ -4,16 +4,21 @@ var uc = require('natural');
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost:3306",
-  user: "root",
-  password: "CS360"
+  host: "localhost",
+  user: "dbProject",
+  password: "password"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+  if (err) throw err;
+  console.log("Database created");
+});
 });
 
+// http://localhost:8080
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The keys: " + dt.myOptionalQuery());
